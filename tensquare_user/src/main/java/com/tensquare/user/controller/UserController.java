82731ba1c5605952c50658afbe87210d26637aa9
Liammi <572645230@qq.com>
2021-03-01 22:21:06 +0800

    Add User Model

---
+++ tensquare_user/src/main/java/com/tensquare/user/controller/UserController.java
@@ -0,0 +1,32 @@
+package com.tensquare.user.controller;
+
+import com.tensquare.user.pojo.User;
+import com.tensquare.user.service.UserService;
+import entity.Result;
+import entity.StatusCode;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("user")
+@CrossOrigin
+public class UserController {
+    @Autowired
+    UserService userService;
+
+    @PostMapping("login")
+    public Result login(@RequestBody User user) {
+        User result = userService.login(user);
+        if (result != null) {
+            return new Result(true, StatusCode.OK, "登录成功", result);
+        }
+        return new Result(false, StatusCode.OK, "登录失败");
+    }
+
+    @GetMapping("{userId}")
+    public Result selectById(@PathVariable String userId) {
+        User user = userService.selectById(userId);
+        return new Result(true, StatusCode.OK, "查询成功", user);
+    }
+}
